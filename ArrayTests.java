a760cdd63be5eeed6a7b665d969759e518084228
nathhanwong <wongnathan17@gmail.com>
2023-10-24 17:27:48 -0700

    lab3inclass

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,15 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = {2, 4, 6, 8 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{8, 6, 4, 2}, input1);
 	}
-
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {4, 5};
+    assertArrayEquals(new int[]{5, 4}, ArrayExamples.reversed(input1));
   }
+
 }
